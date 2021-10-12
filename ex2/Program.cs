4ee87c6f34c997233251a12b579d2831cfa9bd64
Kireev <Kireev@DESKTOP-PF4DRIV>
2021-10-12 09:46:38 +0300

    Add project files.

---
+++ ex2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace ex2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N;
+            float x, y;
+            Console.Write("X: ");
+            x = float.Parse((Console.ReadLine()));
+            Console.Write("Y: ");
+            y = float.Parse((Console.ReadLine()));
+            if (y < 0)
+            {
+                if (y + x * x < 2)
+                {
+                    if (x >= 0) N = 2;
+                    else N = 1;
+                }
+                else
+                {
+                    if (x >= 0) N = 4;
+                    else N = 3;
+                }
+            }
+            else
+            {
+                if (x >= 0) N = 3;
+                else N = 2;
+            }
+            Console.WriteLine($"Result: \nPoint M({x}, {y}) Lies in the area with N = {N}");
+            Console.ReadKey();
+        }
+
+    }
+}
---
+++ laba4_oop_2kurs/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace laba4_oop_2kurs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double func, min, max, c;
+        // ввід значень
+        metka1: Console.Write("Enter the x value: ");
+            double x = double.Parse((Console.ReadLine()));
+            Console.Write("Enter the y value: ");
+            double y = double.Parse((Console.ReadLine()));
+            Console.Write("Enter the z value: ");
+            double z = double.Parse((Console.ReadLine()));
+            // пошук максимального значення х і у
+            if (x > y) max = x;
+            else max = y;
+            // пошук мінімального значення х і у
+            if (x < y + z) min = x;
+            else min = y + z;
+
+            c = max - y;
+            if (c == 0)
+            {
+                Console.WriteLine("The denominator is zero");
+                goto metka1;
+            }
+            else func = min / max - y;
+
+            Console.WriteLine($"X={x}, Y={y}, Z={z}\n Result: F = {func}");
+            Console.ReadKey();
+        }
+    }
+}
